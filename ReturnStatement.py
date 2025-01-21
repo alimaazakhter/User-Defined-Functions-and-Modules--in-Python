fa75b4aebb602784b7e9a00b246b3903cd8ac116
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-21 08:44:32 +0530

    Add files via upload

---
+++ ReturnStatement.py
@@ -0,0 +1,11 @@
+#Return statement in funtions (This keyword is used to exit a funtion and return a value of the funtion)
+def hello():
+    return('Hello world')
+
+print(hello())    
+
+
+def add(a,b):
+    return("The addition of the two number is :", a+b)
+
+print(add(12,3))    
